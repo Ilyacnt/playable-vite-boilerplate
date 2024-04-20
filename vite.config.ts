0bbca466b8a2d22ccc9b23d85c97bdb1dbb401ef
Ilyacnt <ilyagt2012@gmail.com>
2024-04-20 07:27:17 +0300

    ZIP on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
+import zipPack from 'vite-plugin-zip-pack'
+import path from 'node:path'
+
+const currentDir = path.basename(process.cwd())
 
 export default defineConfig({
-    plugins: [viteSingleFile()],
+    plugins: [viteSingleFile(), zipPack({ outDir: 'dist', outFileName: `${currentDir}.zip` })],
 })
